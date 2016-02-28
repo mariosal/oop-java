b7a9ab5608c5b1241daeb3f79a1b55c64f6bab9b
Mario Saldinger <mariosaldinger@gmail.com>
2016-02-29 00:09:56 +0200

    Added initial main class

--- src/Assign.java
+++ src/Assign.java
@@ -1,4 +1,23 @@
 class Assign {
   public static void main(String[] args) {
+    Plane plane = new Plane("747", "Boeing", 5);
+    CleaningEmployee cleaningEmployee = new CleaningEmployee("");
+    MaintenanceEmployee maintenanceEmployee = new MaintenanceEmployee("");
+    SecurityEmployee securityEmployee = new SecurityEmployee("");
+
+    plane.process(cleaningEmployee);
+    cleaningEmployee.report();
+    System.out.println();
+
+    plane.process(maintenanceEmployee);
+    maintenanceEmployee.report();
+    System.out.println();
+
+    plane.process(securityEmployee);
+    securityEmployee.report();
+    System.out.println();
+
+    plane.check();
+    System.out.println("Plane ready to take off!");
   }
 }
