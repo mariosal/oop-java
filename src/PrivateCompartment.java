40bfdc1e88b5a6cc9f92f58d406f594fc786495c
Mario Saldinger <mariosaldinger@gmail.com>
2016-03-03 19:22:09 +0200

    PrivateCompartment: Should be abstract

--- src/PrivateCompartment.java
+++ src/PrivateCompartment.java
@@ -1,13 +1,5 @@
-class PrivateCompartment extends PlaneComponent {
+abstract class PrivateCompartment extends PlaneComponent {
   PrivateCompartment(String description) {
     super(description);
-  }
-
-  @Override void check() {
-    System.out.println("Private Compartment: " + getDescription());
-    System.out.println("Private Compartment OK!");
-  }
-  @Override void process(Employee employee) {
-    employee.work(this);
   }
 }
